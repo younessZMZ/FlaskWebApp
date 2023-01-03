8dac58c7c0d01f84ef8c5189f864b039721da0a3
younessZMZ <younes.zmz@gmail.com>
2023-01-03 21:53:14 +0100

    Format python code with black

--- app.py
+++ app.py
@@ -5,145 +5,144 @@ from flask import Flask, abort, render_template, request, url_for, flash, redire
 from forms import CourseForm
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = '3dba5e7ffcad79a69098833e0448bab8970b80f7bf10f5fd'
+app.config["SECRET_KEY"] = "3dba5e7ffcad79a69098833e0448bab8970b80f7bf10f5fd"
 
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('404.html'), 404
+    return render_template("404.html"), 404
 
 
 @app.errorhandler(500)
 def internal_error(error):
-    return render_template('500.html'), 500
+    return render_template("500.html"), 500
 
 
-@app.route('/500')
+@app.route("/500")
 def error500():
     abort(500)
 
 
-messages = [{'title': 'Message One',
-             'content': 'Message One Content'},
-            {'title': 'Message Two',
-             'content': 'Message Two Content'}
-            ]
+messages = [
+    {"title": "Message One", "content": "Message One Content"},
+    {"title": "Message Two", "content": "Message Two Content"},
+]
 
 
-@app.route('/')
+@app.route("/")
 def hello():
-    return render_template('index.html', messages=messages)
+    return render_template("index.html", messages=messages)
 
 
-@app.route('/about/')
+@app.route("/about/")
 def about():
-    return render_template('about.html')
+    return render_template("about.html")
 
 
-@app.route('/comments/')
+@app.route("/comments/")
 def comments():
-    comments = ['This is the first comment.',
-                'This is the second comment.',
-                'This is the third comment.',
-                'This is the fourth comment.'
-                ]
+    comments = [
+        "This is the first comment.",
+        "This is the second comment.",
+        "This is the third comment.",
+        "This is the fourth comment.",
+    ]
 
-    return render_template('comments.html', comments=comments)
+    return render_template("comments.html", comments=comments)
 
 
-@app.route('/messages/<int:idx>')
+@app.route("/messages/<int:idx>")
 def message(idx):
-    app.logger.info('Building the messages list...')
-    messages = ['Message Zero', 'Message One', 'Message Two']
+    app.logger.info("Building the messages list...")
+    messages = ["Message Zero", "Message One", "Message Two"]
     try:
-        app.logger.debug('Get message with index: {}'.format(idx))
-        return render_template('message.html', message=messages[idx])
+        app.logger.debug("Get message with index: {}".format(idx))
+        return render_template("message.html", message=messages[idx])
     except IndexError:
-        app.logger.error('Index {} is causing an IndexError'.format(idx))
+        app.logger.error("Index {} is causing an IndexError".format(idx))
         abort(404)
 
 
-@app.route('/capitalize/<word>/')
+@app.route("/capitalize/<word>/")
 def capitalize(word):
-    return '<h1>{}</h1>'.format(escape(word.capitalize()))
+    return "<h1>{}</h1>".format(escape(word.capitalize()))
 
 
-@app.route('/add/<int:n1>/<int:n2>/')
+@app.route("/add/<int:n1>/<int:n2>/")
 def add(n1, n2):
-    return '<h1>{}</h1>'.format(n1 + n2)
+    return "<h1>{}</h1>".format(n1 + n2)
 
 
-@app.route('/users/<int:user_id>/')
+@app.route("/users/<int:user_id>/")
 def greet_user(user_id):
-    users = ['Bob', 'Jane', 'Adam']
+    users = ["Bob", "Jane", "Adam"]
     try:
-        return '<h2>Hi {}</h2>'.format(users[user_id])
+        return "<h2>Hi {}</h2>".format(users[user_id])
     except IndexError:
         abort(404)
 
 
 # setting up forms
-@app.route('/create/', methods=('GET', 'POST'))
+@app.route("/create/", methods=("GET", "POST"))
 def create():
-    if request.method == 'POST':
-        title = request.form['title']
-        content = request.form['content']
+    if request.method == "POST":
+        title = request.form["title"]
+        content = request.form["content"]
 
         if not title:
-            flash('Title is required!')
+            flash("Title is required!")
         elif not content:
-            flash('Content is required!')
+            flash("Content is required!")
         else:
-            messages.append({'title': title, 'content': content})
-            return redirect(url_for('hello'))
-    return render_template('create.html')
+            messages.append({"title": title, "content": content})
+            return redirect(url_for("hello"))
+    return render_template("create.html")
 
 
 # adding forms with WTF-Forms
-courses_list = [{
-    'title': 'Python 101',
-    'description': 'Learn Python basics',
-    'price': 34,
-    'available': True,
-    'level': 'Beginner'
-    }]
+courses_list = [
+    {"title": "Python 101", "description": "Learn Python basics", "price": 34, "available": True, "level": "Beginner"}
+]
 
 
-@app.route('/form', methods=('GET', 'POST'))
+@app.route("/form", methods=("GET", "POST"))
 def form():
     form = CourseForm()
     if form.validate_on_submit():
-        courses_list.append({'title': form.title.data,
-                             'description': form.description.data,
-                             'price': form.price.data,
-                             'available': form.available.data,
-                             'level': form.level.data
-                             })
-        return redirect(url_for('courses'))
-    return render_template('form.html', form=form)
+        courses_list.append(
+            {
+                "title": form.title.data,
+                "description": form.description.data,
+                "price": form.price.data,
+                "available": form.available.data,
+                "level": form.level.data,
+            }
+        )
+        return redirect(url_for("courses"))
+    return render_template("form.html", form=form)
 
 
-@app.route('/courses/')
+@app.route("/courses/")
 def courses():
-    return render_template('courses.html', courses_list=courses_list)
+    return render_template("courses.html", courses_list=courses_list)
 
 
 # connecting and inserting data to sqlite database
 def get_db_connection():
-    conn = sqlite3.connect('database.db')
+    conn = sqlite3.connect("database.db")
     conn.row_factory = sqlite3.Row
     return conn
 
 
-@app.route('/sqlite')
+@app.route("/sqlite")
 def index():
     conn = get_db_connection()
-    posts = conn.execute('SELECT * FROM posts').fetchall()
+    posts = conn.execute("SELECT * FROM posts").fetchall()
     conn.close()
-    return render_template('sqlite_index.html', posts=posts)
+    return render_template("sqlite_index.html", posts=posts)
 
 
-@app.route('/sql_create/', methods=('GET', 'POST'))
+@app.route("/sql_create/", methods=("GET", "POST"))
 def sql_create():
     if request.method == "POST":
         title = request.form["title"]
@@ -159,4 +158,4 @@ def sql_create():
             conn.commit()
             conn.close()
             return redirect(url_for("index"))
-    return render_template('sql_create.html')
+    return render_template("sql_create.html")
--- init_db.py
+++ init_db.py
@@ -1,20 +1,22 @@
 import sqlite3
 
-connection = sqlite3.connect('database.db')
+connection = sqlite3.connect("database.db")
 
 
-with open('schema.sql') as f:
+with open("schema.sql") as f:
     connection.executescript(f.read())
 
 cur = connection.cursor()
 
-cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
-            ('First Post', 'Content for the first post')
-            )
+cur.execute(
+    "INSERT INTO posts (title, content) VALUES (?, ?)",
+    ("First Post", "Content for the first post"),
+)
 
-cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
-            ('Second Post', 'Content for the second post')
-            )
+cur.execute(
+    "INSERT INTO posts (title, content) VALUES (?, ?)",
+    ("Second Post", "Content for the second post"),
+)
 
 connection.commit()
 connection.close()
